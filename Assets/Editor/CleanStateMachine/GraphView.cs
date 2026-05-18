1092d831bcae3be054547e18cb2ea76344045c3a
MohammadHassan <mhze.uk@gmail.com>
2026-05-18 23:23:04 +0100

    removed annoying graph crosshairs

--- Assets/Editor/CleanStateMachine/GraphView.cs
+++ Assets/Editor/CleanStateMachine/GraphView.cs
@@ -8,11 +8,8 @@ namespace CleanStateMachine
         private static readonly Color Bg = new Color(0.100f, 0.100f, 0.100f);
         private static readonly Color GridMinor = new Color(0.120f, 0.120f, 0.120f);
         private static readonly Color GridMajor = new Color(0.150f, 0.150f, 0.150f);
-        private static readonly Color Crosshair = new Color(0.180f, 0.180f, 0.180f, 0.350f);
-
         private const float GridS = 20f;
         private const float GridL = 100f;
-        private const float CrosshairSize = 4f;
 
         public void Draw(Rect rect, Vector2 panOffset, float zoom)
         {
@@ -21,10 +18,8 @@ namespace CleanStateMachine
             float thickness = Mathf.Max(1f, zoom);
             float sGridS = GridS * zoom;
             float sGridL = GridL * zoom;
-            float sCross = CrosshairSize * zoom;
 
             DrawGridLines(rect, panOffset, sGridS, sGridL, thickness);
-            DrawCrosshairs(rect, panOffset, sGridL, sCross);
         }
 
         private static void DrawGridLines(Rect rect, Vector2 panOffset, float sGridS, float sGridL, float thickness)
@@ -50,21 +45,5 @@ namespace CleanStateMachine
                 EditorGUI.DrawRect(new Rect(0f, y, rect.width, thickness), GridMajor);
         }
 
-        private static void DrawCrosshairs(Rect rect, Vector2 panOffset, float sGridL, float sCross)
-        {
-            float ox = panOffset.x % sGridL;
-            float oy = panOffset.y % sGridL;
-            if (ox < 0f) ox += sGridL;
-            if (oy < 0f) oy += sGridL;
-
-            for (float x = ox; x < rect.width; x += sGridL)
-            {
-                for (float y = oy; y < rect.height; y += sGridL)
-                {
-                    EditorGUI.DrawRect(new Rect(x - sCross, y - 1f, sCross * 2f, 2f), Crosshair);
-                    EditorGUI.DrawRect(new Rect(x - 1f, y - sCross, 2f, sCross * 2f), Crosshair);
-                }
-            }
-        }
     }
 }
